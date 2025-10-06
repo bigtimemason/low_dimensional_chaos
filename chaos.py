495ac25e3f68cd58feb779c38818f677048e1cdb
Mason Miller <44513924+bigtimemason@users.noreply.github.com>
2025-10-06 12:59:17 -0400

    Update chaos.py

--- chaos.py
+++ chaos.py
@@ -1,2 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+# ========================================
+# s = array containing x(t), y(t), z(t)
+# returns and array containing time derivatives of x,y,z
+# ----------------------------------------
+def dsdt(s):
+    
+    dsdt = None
+    return dsdt
+
+
+def main():
+    pass
+
+
+
+main()
