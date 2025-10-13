d86385d4b3a4f2465f5968d4b52a3bed06c0ef7e
Mason Miller <44513924+bigtimemason@users.noreply.github.com>
2025-10-13 15:49:14 -0400

    Update ode_integrators.py
    functional now

--- ode_integrators.py
+++ ode_integrators.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-def ode_ivp(fRHS,fORD,fBVP,x0,y0,x1,nstep):
+def ode_ivp(fRHS,fORD,t0,s0,t1,nstep):
 
-    nvar    = y0.size                      # number of ODEs
-    x       = np.linspace(x0,x1,nstep+1)   # generates equal-distant support points
-    y       = np.zeros((nvar,nstep+1))     # result array 
-    y[:,0]  = y0                           # set initial condition
-    dx      = (x[1]-x[0])                  # step size
+    nvar    = s0.size                      # number of ODEs
+    t       = np.linspace(t0,t1,nstep+1)   # generates equal-distant support points
+    s       = np.zeros((nvar,nstep+1)) 
+    s[:,0]  = s0                         
+    dt      = 1e-3                         # ???? step size - IMPLAMENT ADAPTIVE STEP SIZE
     it      = np.zeros(nstep+1)
     for k in range(1,nstep+1):
-        y[:,k],it[k] = fORD(fRHS,x[k-1],y[:,k-1],dx)
-    return x,y,it
+        s[:,k],it[k] = fORD(fRHS,t[k-1],s[:,k-1],dt)
+    return t,s,it
