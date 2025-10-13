1d9307507c628a7e4f955820c9e9887794dec3c4
Mason Miller <44513924+bigtimemason@users.noreply.github.com>
2025-10-13 14:06:58 -0400

    Update ode_step.py
    rk4 added

--- ode_step.py
+++ ode_step.py
@@ -1,1 +1,16 @@
 import numpy as np
+import ode_integrators
+import chaos_p1
+import chaos_p2
+
+
+def rk4(fRHS,x0,y0,dx):
+    #???????? from here
+    k1 = dx*fRHS(x0,y0,dx)
+    k2 = dx*fRHS(x0 + dx/2,y0 + k1/2,dx)
+    k3 = dx*fRHS(x0 + dx/2,y0 + k2/2,dx)
+    k4 = dx*fRHS(x0 + dx/1,y0 + k3/1,dx)
+    
+    y = y0 + (k1+k4)/6 + (k2+k3)/3
+    #???????? to here
+    return y,1
