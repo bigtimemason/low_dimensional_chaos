4ba58ac19a5fc60193a3f64d6484b4078d60c46f
Mason Miller <44513924+bigtimemason@users.noreply.github.com>
2025-10-13 15:50:42 -0400

    Update ode_step.py

--- ode_step.py
+++ ode_step.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 def rk4(fRHS,t,s0,dt):
-    #???????? from here
+
     k1 = dt*fRHS(t,s0,dt)
     k2 = dt*fRHS(t + dt/2,s0 + k1/2,dt)
     k3 = dt*fRHS(t + dt/2,s0 + k2/2,dt)
     k4 = dt*fRHS(t + dt/1,s0 + k3/1,dt)
     
     s = s0 + (k1+k4)/6 + (k2+k3)/3
-    #???????? to here
+
     return s,1
