a42745a766bed0ca06f4175ff8a06dc4c7fe09af
Mason Miller <44513924+bigtimemason@users.noreply.github.com>
2025-10-21 14:33:04 -0400

    Update ode_integrators.py

--- ode_integrators.py
+++ ode_integrators.py
@@ -6,8 +6,9 @@ def ode_ivp(fRHS,fORD,t0,s0,t1,nstep):
     t       = np.linspace(t0,t1,nstep+1)   # generates equal-distant support points
     s       = np.zeros((nvar,nstep+1)) 
     s[:,0]  = s0                         
-    dt      = 1e-3                         # ???? step size - IMPLAMENT ADAPTIVE STEP SIZE
+    dt      = 1e-4                    # ???? step size - IMPLAMENT ADAPTIVE STEP SIZE
     it      = np.zeros(nstep+1)
+    
     for k in range(1,nstep+1):
         s[:,k],it[k] = fORD(fRHS,t[k-1],s[:,k-1],dt)
     return t,s,it
